95d1daafa8ddf07757163ceaf8c0a37f0ec5ee11
Hans Liss <Hans@Liss.nu>
2008-07-28 21:50:38 +0000

    Added copyright
    git-svn-id: https://svn.liss.nu/svn/projects/gluff/trunk@95 5348baa3-93bb-4dff-ae79-93250e8dd5e6

--- gluff.c
+++ gluff.c
@@ -1,6 +1,12 @@
 /*
  * gluff - eat DHCPACK events from a queue table written to by dhcpd, and write out complete
  *         lease records to a mysql database.
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version
+ *   2 of the License, or (at your option) any later version.
+ *
  * Copyright: Hans Liss - Hans@Liss.pp.se 2008
  *
  */
